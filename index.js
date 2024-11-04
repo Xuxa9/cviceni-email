1871609342b9acbd277194bf74d72ca7fdc2a926
Zuzana Čechová <zu.cechova9@gmail.com>
2024-11-04 20:51:20 +0100

    ukol hotovy

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+const goodbye = function(name){ 
+    const pozdrav = document.querySelector(".email__closing")
+    pozdrav.textContent = `
+    Na shledanou ${name} 
+    `
+} 
+goodbye("Zuzana")
